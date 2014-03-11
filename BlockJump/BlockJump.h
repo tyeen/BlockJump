30109f5d56d25b77fbe4f430ed169bd5675aa02b
tyeen <ptyeen@gmail.com>
2014-03-11 11:44:49 +0900

    Setup for plugin developing.

---
+++ BlockJump/BlockJump.h
@@ -0,0 +1,13 @@
+//
+//  Demo.h
+//  BlockJump
+//
+//  Created by Yin Tan on 3/11/14.
+//  Copyright (c) 2014 Yin Tan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlockJump : NSObject
+
+@end
---
+++ BlockJump/BlockJump.m
@@ -0,0 +1,18 @@
+//
+//  Demo.m
+//  BlockJump
+//
+//  Created by Yin Tan on 3/11/14.
+//  Copyright (c) 2014 Yin Tan. All rights reserved.
+//
+
+#import "BlockJump.h"
+
+@implementation BlockJump
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+  NSLog(@"demo loaded.");
+}
+
+@end
